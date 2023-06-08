b597dba9da9bad0ededf4cd6a4cb18d37979a378
rakinrkz <rakinuzzaman@gmail.com>
2023-06-08 15:45:27 +0600

    modified arm code for arduino mega

--- rover-arm/rover-arm.ino
+++ rover-arm/rover-arm.ino
@@ -1,78 +1,66 @@
-#include <SoftwareSerial.h>
 #include "Actuator.h"
 #include <Servo.h>
 
 Actuator primary(10, 11);
-Actuator secondary(3, 5);
-Actuator wrist(6, 9);
+Actuator secondary(9, 8);
+Actuator wrist(6, 7);
+Actuator gripper(5, 4);
 
-Servo gripperServo;
-#define gripperServoPin 2
 
-//RC channels: 
+//RC channels:
 #define primaryChannel 3
 #define secondaryChannel 2
 #define wristChannel 7
 #define gripperChannel 8
 
 #define armed data[5] == '3'
-#define DEBUG false
+#define DEBUG true
 
 String data;
 bool received = false;
 byte channelAmount = 8;
 
-SoftwareSerial ss1(A4, A5);
 
 void setup() {
-  Serial.begin(115200);
-  ss1.begin(4800);
-  gripperServo.attach(gripperServoPin);
-
-  delay(4000);
+  Serial.begin(9600);
+  Serial3.begin(4800);
+  Serial.println("started");
 }
 
 void loop() {
   receive();
+  if (armed) {
+    manipulate();
+  } else
+    halt();
 }
 
-void useGripper(int val) {
-  if (val == 1)
-    gripperServo.writeMicroseconds(1100);
-  else if (val == 3)
-    gripperServo.writeMicroseconds(1600);
-  else
-    gripperServo.writeMicroseconds(1400);
-}
 
 void manipulate() {
   primary.movement(data[primaryChannel] - '0');
   secondary.movement(data[secondaryChannel] - '0');
   wrist.movement(data[wristChannel] - '0');
-  useGripper(data[gripperChannel] - '0');
+  gripper.movement(data[gripperChannel] - '0');
 }
 
 void halt() {
   primary.movement(2);
   secondary.movement(2);
   wrist.movement(2);
+  gripper.movement(2);
 }
 
 void receive() {
   data = "";
   received = false;
-  while (ss1.available()) {
-    data = ss1.readStringUntil('E');
+  while (Serial3.available()) {
+    data = Serial3.readStringUntil('E');
     if (DEBUG)
       Serial.println(data);
     received = true;
   }
   if (DEBUG and !received) {
-    // Serial.println("no data recieved");
-    // delay(100);
+    Serial.println("no data recieved");
+    delay(100);
   }
-  if (armed) {
-    manipulate();
-  } else if (!armed)
-    halt();
 }
